b3a91baaa652efa11956c944dfdc9f6d422fdf6c
senik <farsyan.senik@gmail.com>
2018-11-19 11:36:43 +0400

    Updates

---
+++ precache-manifest.073eb291dece404a1e56ae0b00ad1ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfabf8157d81ca4b1c24",
+    "url": "/react_lyric_search/static/js/runtime~main.bfabf815.js"
+  },
+  {
+    "revision": "fa7ef586810d18179b30",
+    "url": "/react_lyric_search/static/js/main.fa7ef586.chunk.js"
+  },
+  {
+    "revision": "b98963745c0e2d522b6b",
+    "url": "/react_lyric_search/static/js/1.b9896374.chunk.js"
+  },
+  {
+    "revision": "fa7ef586810d18179b30",
+    "url": "/react_lyric_search/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "606d3ad43f75730c304417a33604eb96",
+    "url": "/react_lyric_search/index.html"
+  }
+];
